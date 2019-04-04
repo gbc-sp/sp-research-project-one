382ac72420cf5e4d2168510bef4d243c080f9af8
Aaron <aaron@deroche.design>
2019-04-04 18:18:49 -0400

    Update CurrentPortfolioPiece.js

--- src/components/portfolio/CurrentPortfolioPiece.js
+++ src/components/portfolio/CurrentPortfolioPiece.js
@@ -2,39 +2,35 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import back_arrow from "icons/back-arrow.svg";
-
-class CurrentPortfolioPiece extends Component {
-  state = {
-    piece: this.props.piece
-  };
-
-  render() {
-    return (
-      <div className="current-portfolio-piece">
-        <div className="tile full no-padding">
-          <div className="profile-image">
-            <div>
-              <img alt="portfolio piece image" src={this.state.piece.img} />
-            </div>
-          </div>
-          <div className="tile blank">
-            <Link to="null:">
-              <img alt="page back icon" className="go-back" src={back_arrow} />
-            </Link>
-            <h1 className="no-vertical-margin">{this.state.piece.title}</h1>
-            <h2 className="no-vertical-margin">
-              {this.state.piece.skill.name}
-            </h2>
-            <p>{this.state.piece.content}</p>
-            {/* This would go back to portfolio piece selection. it only shows if a piece has been selected. */}
-            <Link to="null:" className="button">
-              Go Back
-            </Link>
+const { useState} = React;
+const CurrentPortfolioPiece = props => {
+  const [piece, setPiece] = useState();
+  
+  return (
+    <div className="current-portfolio-piece">
+      <div className="tile full no-padding">
+        <div className="profile-image">
+          <div>
+            <img alt="portfolio piece image" src={piece.img} />
           </div>
         </div>
+        <div className="tile blank">
+          <Link to="null:">
+            <img alt="page back icon" className="go-back" src={back_arrow} />
+          </Link>
+          <h1 className="no-vertical-margin">{piece.title}</h1>
+          <h2 className="no-vertical-margin">
+            {piece.skill.name}
+          </h2>
+          <p>{piece.content}</p>
+          {/* This would go back to portfolio piece selection. it only shows if a piece has been selected. */}
+          <Link to="null:" className="button">
+            Go Back
+          </Link>
+        </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default CurrentPortfolioPiece;
